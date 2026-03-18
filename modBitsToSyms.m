d95024194940ea7639d43cc4e9b5afe2caa6823c
WENS <96454461+WENS-KIT@users.noreply.github.com>
2026-03-18 11:09:21 +0900

    Delete modBitsToSyms.m

--- modBitsToSyms.m
+++
@@ -1,7 +0,0 @@
-function X = modBitsToSyms(bits, Mod, ModType)
-if ModType == "PSK"
-    X = pskmod(bits, Mod, pi/4, InputType="bit");
-else
-    X = qammod(bits, Mod, 'InputType','bit', 'UnitAveragePower', true);
-end
-end
